e61bb8d9845cbf85be4787737638a80f93d63003
Sungki <youbyme@gmail.com>
2021-04-17 16:08:02 -0400

    Draw Stars and compiled it for Web

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,125 @@
+#ifdef __EMSCRIPTEN__
+#include "emscripten.h"
+#endif
+#include <iostream>
+#include <vector>
+
 #include "SDL.h"
 #undef main
 
 #include "imgui.h"
 #include "imgui_sdl.h"
 
+struct Vector2
+{
+	float x;
+	float y;
+};
+
+struct sStar
+{
+	float fAngle = 0.0f;
+	float fDistance = 0.0f;
+	float fSpeed = 0.0f;
+};
+
+bool run;
+SDL_Renderer* renderer;
+SDL_Texture* texture;
+const int nStars = 500;
+std::vector<sStar> vStars;
+Vector2 vOrigin;
+
+Uint64 NOW = SDL_GetPerformanceCounter();
+Uint64 LAST = 0;
+double deltaTime = 0;
+
+float Random(float a, float b)
+{
+	return (b - a) * (float(rand()) / float(RAND_MAX)) + a;
+}
+
+void mainloop()
+{
+	LAST = NOW;
+	NOW = SDL_GetPerformanceCounter();
+
+	deltaTime = (double)((NOW - LAST) * 1000 / (double)SDL_GetPerformanceFrequency());
+
+	ImGuiIO& io = ImGui::GetIO();
+
+	int wheel = 0;
+
+	SDL_Event e;
+	while (SDL_PollEvent(&e))
+	{
+		if (e.type == SDL_QUIT) run = false;
+		else if (e.type == SDL_WINDOWEVENT)
+		{
+			if (e.window.event == SDL_WINDOWEVENT_SIZE_CHANGED)
+			{
+					io.DisplaySize.x = static_cast<float>(e.window.data1);
+					io.DisplaySize.y = static_cast<float>(e.window.data2);
+			}
+		}
+		else if (e.type == SDL_MOUSEWHEEL)
+		{
+			wheel = e.wheel.y;
+		}
+	}
+
+	int mouseX, mouseY;
+	const int buttons = SDL_GetMouseState(&mouseX, &mouseY);
+
+	io.DeltaTime = 1.0f / 60.0f;
+	io.MousePos = ImVec2(static_cast<float>(mouseX), static_cast<float>(mouseY));
+	io.MouseDown[0] = buttons & SDL_BUTTON(SDL_BUTTON_LEFT);
+	io.MouseDown[1] = buttons & SDL_BUTTON(SDL_BUTTON_RIGHT);
+	io.MouseWheel = static_cast<float>(wheel);
+
+	ImGui::NewFrame();
+
+	ImGui::ShowDemoWindow();
+
+	ImGui::Begin("Image");
+	ImGui::Image(texture, ImVec2(100, 100));
+	ImGui::End();
+
+	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+	SDL_RenderClear(renderer);
+
+	for (auto& star : vStars)
+	{
+		star.fDistance += star.fSpeed * deltaTime * (star.fDistance / 100.0f);
+		if (star.fDistance > 1000.0f)
+		{
+			star.fAngle = Random(0.0f, 2.0f * 3.1459f);
+			star.fSpeed = Random(10.0f, 100.0f);
+			star.fDistance = Random(1.0f, 100.0f);
+		}
+
+		SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+		SDL_RenderDrawPoint(renderer, cos(star.fAngle) * star.fDistance + vOrigin.x, sin(star.fAngle)* star.fDistance + vOrigin.y);
+//		Draw(olc::vf2d(cos(star.fAngle), sin(star.fAngle)) * star.fDistance + vOrigin, star.col * (star.fDistance / 100.0f));
+	}
+
+	ImGui::Render();
+	ImGuiSDL::Render(ImGui::GetDrawData());
+
+	SDL_RenderPresent(renderer);
+}
+
 int main()
 {
 	SDL_Init(SDL_INIT_EVERYTHING);
 
-	SDL_Window* window = SDL_CreateWindow("SDL2 ImGui Renderer", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_RESIZABLE);
-	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+	SDL_Window* window = SDL_CreateWindow("WebGuiApp", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_RESIZABLE);
+	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
 	ImGui::CreateContext();
 	ImGuiSDL::Initialize(renderer, 800, 600);
 
-	SDL_Texture* texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA32, SDL_TEXTUREACCESS_TARGET, 100, 100);
+	texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA32, SDL_TEXTUREACCESS_TARGET, 100, 100);
 	{
 		SDL_SetRenderTarget(renderer, texture);
 		SDL_SetRenderDrawColor(renderer, 255, 0, 255, 255);
@@ -22,57 +127,24 @@ int main()
 		SDL_SetRenderTarget(renderer, nullptr);
 	}
 
-	bool run = true;
+	vStars.resize(nStars);
+
+	for (auto& star : vStars)
+	{
+		star.fAngle = Random(0.0f, 2.0f * 3.1459f);
+		star.fSpeed = Random(10.0f, 100.0f);
+		star.fDistance = Random(1.0f, 100.0f);
+	}
+
+	vOrigin = { 400, 300 };
+
+#ifdef __EMSCRIPTEN__
+	emscripten_set_main_loop(mainloop, 0, 0);
+#else
+	run = true;
 	while (run)
 	{
-		ImGuiIO& io = ImGui::GetIO();
-
-		int wheel = 0;
-
-		SDL_Event e;
-		while (SDL_PollEvent(&e))
-		{
-			if (e.type == SDL_QUIT) run = false;
-			else if (e.type == SDL_WINDOWEVENT)
-			{
-				if (e.window.event == SDL_WINDOWEVENT_SIZE_CHANGED)
-				{
-					io.DisplaySize.x = static_cast<float>(e.window.data1);
-					io.DisplaySize.y = static_cast<float>(e.window.data2);
-				}
-			}
-			else if (e.type == SDL_MOUSEWHEEL)
-			{
-				wheel = e.wheel.y;
-			}
-		}
-
-		int mouseX, mouseY;
-		const int buttons = SDL_GetMouseState(&mouseX, &mouseY);
-
-		// Setup low-level inputs (e.g. on Win32, GetKeyboardState(), or write to those fields from your Windows message loop handlers, etc.)
-
-		io.DeltaTime = 1.0f / 60.0f;
-		io.MousePos = ImVec2(static_cast<float>(mouseX), static_cast<float>(mouseY));
-		io.MouseDown[0] = buttons & SDL_BUTTON(SDL_BUTTON_LEFT);
-		io.MouseDown[1] = buttons & SDL_BUTTON(SDL_BUTTON_RIGHT);
-		io.MouseWheel = static_cast<float>(wheel);
-
-		ImGui::NewFrame();
-
-		ImGui::ShowDemoWindow();
-
-		ImGui::Begin("Image");
-		ImGui::Image(texture, ImVec2(100, 100));
-		ImGui::End();
-
-		SDL_SetRenderDrawColor(renderer, 114, 144, 154, 255);
-		SDL_RenderClear(renderer);
-
-		ImGui::Render();
-		ImGuiSDL::Render(ImGui::GetDrawData());
-
-		SDL_RenderPresent(renderer);
+		mainloop();
 	}
 
 	ImGuiSDL::Deinitialize();
@@ -81,6 +153,7 @@ int main()
 	SDL_DestroyWindow(window);
 
 	ImGui::DestroyContext();
+#endif
 
 	return 0;
 }
