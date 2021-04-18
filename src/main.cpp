aa24cd3d8c832cb71f8bcd67a99223e91a21dc19
Sungki <youbyme@gmail.com>
2021-04-18 13:42:25 -0400

    Added PixelGame header file

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-#ifdef __EMSCRIPTEN__
+/*#ifdef __EMSCRIPTEN__
 #include "emscripten.h"
 #endif
 #include <iostream>
@@ -155,5 +155,36 @@ int main()
 	ImGui::DestroyContext();
 #endif
 
+	return 0;
+}*/
+
+#define OLC_PGE_APPLICATION
+#include "olcPixelGameEngineSDL.h"
+
+class Example : public olc::PixelGameEngine
+{
+public:
+	Example()
+	{
+		sAppName = "Example";
+	}
+public:
+	bool OnUserCreate() override
+	{
+		return true;
+	}
+	bool OnUserUpdate(float fElapsedTime) override
+	{
+		for (int x = 0; x < ScreenWidth(); x++)
+			for (int y = 0; y < ScreenHeight(); y++)
+				Draw(x, y, olc::Pixel(rand() % 255, rand() % 255, rand() % 255));
+		return true;
+	}
+};
+int main()
+{
+	Example demo;
+	if (demo.Construct(256, 240, 4, 4))
+		demo.Start();
 	return 0;
 }
