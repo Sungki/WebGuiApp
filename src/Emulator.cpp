458d38bdea37407b3a6621bb0c95cb0caf484a3a
Sungki <youbyme@gmail.com>
2021-06-30 16:36:27 -0400

    Added fwrite func

--- src/Emulator.cpp
+++ src/Emulator.cpp
@@ -59,6 +59,10 @@ bool Emulator::LoadRom(const std::string& romName)
 
 	memcpy(&m_Rom[0x0], &m_GameBank[0], 0x8000) ; // this is read only and never changes
 
+//	in = fopen("t.txt", "wb");
+//	fwrite(&m_Rom, 1, sizeof(m_Rom)/2, in);
+//	fclose(in);
+
 	m_CurrentRomBank = 1;
 
 	return true ;
