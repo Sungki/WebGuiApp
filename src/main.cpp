e579fc03193aeb29d4c19fcf6405c823eb1e0b21
Sungki <youbyme@gmail.com>
2021-07-08 12:42:11 -0400

    Draw the frambuffer

--- src/main.cpp
+++ src/main.cpp
@@ -737,26 +737,27 @@ public:
 		int x =0, y=0;
 		int count = 0;
 
-		for (unsigned int i = 0x1800; i < 0x1BFF; i++, x++)
+		for (unsigned int i = 0x1800; i < 0x1A10; i++, x++)
 		{
-			if (x > 32)
-			{
-				x = 0;
-				y++;
-			}
 //			if (priv.Vram[i] == 0x7F)
 //			{
-				if (gb.vram[i] == 0x8C) DrawRect(x*4, y*4, 4, 4, olc::Pixel(0, 0, 100));
-				if (gb.vram[i] == 0x3A) DrawRect(x*4, y*4, 4, 4, olc::Pixel(0, 0, 150));
-				if (gb.vram[i] == 0x3B) DrawRect(x*4, y*4, 4, 4, olc::Pixel(0, 0, 255));
-				if (gb.vram[i] == 0x62) DrawRect(x*4, y*4, 4, 4, olc::Pixel(0, 100, 0));
-				if (gb.vram[i] == 0x63) DrawRect(x*4, y*4, 4, 4, olc::Pixel(0, 150, 0));
-				if (gb.vram[i] == 0x64) DrawRect(x*4, y*4, 4, 4, olc::Pixel(0, 255, 0));
-				if (gb.vram[i] == 0x65) DrawRect(x*4, y*4, 4, 4, olc::Pixel(100, 0, 0));
+				if (gb.vram[i] == 0x8E) DrawRect(x*4, y*4, 4, 4, olc::Pixel(0, 0, 100));
+				if (gb.vram[i] == 0x2F) DrawRect(x*4, y*4, 4, 4, olc::Pixel(0, 100, 0));
+//				if (gb.vram[i] == 0x02) DrawRect(x*4, y*4, 4, 4, olc::Pixel(0, 0, 255));
+//				if (gb.vram[i] == 0x03) DrawRect(x*4, y*4, 4, 4, olc::Pixel(0, 100, 0));
+//				if (gb.vram[i] == 0x04) DrawRect(x*4, y*4, 4, 4, olc::Pixel(0, 150, 0));
+//				if (gb.vram[i] == 0x05) DrawRect(x*4, y*4, 4, 4, olc::Pixel(0, 255, 0));
+//				if (gb.vram[i] == 0x06) DrawRect(x*4, y*4, 4, 4, olc::Pixel(100, 0, 0));
 //				if (priv.Vram[i] == 0x28) DrawRect(x, y, 4, 4, olc::Pixel(150, 0, 0));
 // 				if (priv.Vram[i] == 0x29) DrawRect(x, y, 4, 4, olc::Pixel(255, 0, 0));
 //				count++;
 //			}
+
+			if (x >= 32)
+			{
+				x = 0;
+				y++;
+			}
 		}
 
 /*		for (int y = 0; y < 8; y++)
